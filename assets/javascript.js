ba5220dc8a285f4939e453b9548d5ff015b75828
mujthabamkdev <mujthabamkdev@gmail.com>
2023-03-19 15:16:12 +0530

    events carousel change

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,11 +1,20 @@
-$(document).ready(function() {
-    $('nav a').click(function() {
-      $('nav a').removeClass('active');
-      $(this).addClass('active');
-      $('html, body').animate({
-        scrollTop: $($(this).attr('href')).offset().top
-      }, 500);
-      return false;
+// $(document).ready(function() {
+//     $('nav a').click(function() {
+//       $('nav a').removeClass('active');
+//       $(this).addClass('active');
+//       $('html, body').animate({
+//         scrollTop: $($(this).attr('href')).offset().top
+//       }, 500);
+//       return false;
+//     });
+//   });
+
+  $(document).ready(function() {
+    $('.navbar-collapse').on('shown.bs.collapse', function() {
+        $('.navbar-toggler-icon').removeClass('navbar-toggler-icon').addClass('navbar-close-icon');
     });
-  });
+    $('.navbar-collapse').on('hidden.bs.collapse', function() {
+        $('.navbar-close-icon').removeClass('navbar-close-icon').addClass('navbar-toggler-icon');
+    });
+});
   
